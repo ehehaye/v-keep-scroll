9c0b7e0ae4721b58dedae28b2d6ff8f81f5c9f5c
tanghongxin <hongxin.tang@hotmail.com>
2024-05-08 19:15:53 +0800

    🔖 release v0.0.1
    Squashed commit of the following:
    
    commit c789ca22467a5759ee80454c994668276f157ea9
    Author: tanghongxin <hongxin.tang@hotmail.com>
    Date:   Wed May 8 19:14:49 2024 +0800
    
        ✨ 指令初始化与增加演示案例
    
    commit 381e920cc707d0f487317683066842266d0a4aab
    Author: tanghongxin <hongxin.tang@hotmail.com>
    Date:   Wed May 8 19:13:47 2024 +0800
    
        👷 初始化 vue-demo 相关依赖，迁移 NPM 至 PNPM

---
+++ example/CompB.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="comp-b" v-keep-scroll>
+    <div class="child"></div>
+  </div>
+</template>
+
+<style>
+#comp-b {
+  background-color: cyan;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+</style>
